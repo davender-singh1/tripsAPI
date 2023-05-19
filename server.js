4cae3076998a17989d67baf1d154c14fb35709e9
davender-singh1 <106000634+davender-singh1@users.noreply.github.com>
2023-05-19 18:13:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Davender Singh Student ID: 125700211 Date: 19-05-2023
+*  Cyclic Link: https://pear-aggressive-indri.cyclic.app/
+*
+********************************************************************************/ 
 const HTTP_PORT = process.env.PORT || 8080;
 const express = require("express");
 const cors = require("cors");
